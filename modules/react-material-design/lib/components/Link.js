f07aa4c5a8fe1720c6b440d7736787739f272a6b
Ricky Vetter <ricky@socialtables.com>
2015-09-13 09:58:39 -0400

    get rid of general lodash references

--- modules/react-material-design/lib/components/Link.js
+++ modules/react-material-design/lib/components/Link.js
@@ -9,7 +9,7 @@ function _classCallCheck(instance, Constructor) { if (!(instance instanceof Cons
 function _inherits(subClass, superClass) { if (typeof superClass !== 'function' && superClass !== null) { throw new TypeError('Super expression must either be null or a function, not ' + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
 
 var React = require('react');
-var _ = require('lodash');
+var isString = require('lodash.isstring');
 
 var Link = (function (_React$Component) {
   _inherits(Link, _React$Component);
@@ -39,7 +39,7 @@ var Link = (function (_React$Component) {
     value: function render() {
 
       var a;
-      if (_.isString(this.props.onClick)) {
+      if (isString(this.props.onClick)) {
         a = React.createElement(
           'a',
           { style: { textDecoration: 'none' }, href: this.props.onClick, target: this.props.newTab && '_blank' },
